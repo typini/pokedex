895b608ab03ebaa7d20de28419b716f58515c92b
Tyree Pini <typini@gmail.com>
2019-05-16 09:02:52 -0700

    IIFE incorporated
    I added IIFE to my code.  Now, I'm using a function kind of like a public class to get and set pokemon to my pokedex.  This commit restructures the .js file with a pokemonRepository function with internal method calls as its return.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,86 @@
-let repository = [];
+//KEYARRAY is an array of required keys in a valid pokemon object
+const KEYARRAY = ['name', 'height', 'types'];
 
-repository.push({
+//IIFE = Immediately Invoked Function Expression
+let pokemonRepository = (function () {
+    let pokedex = [];
+
+    function add(pokemon) {
+        //verify input in an object
+        if (typeof pokemon === 'object') {
+            //verify the object has all necessary keys
+            let valid = true;
+            KEYARRAY.forEach(function (key) {
+                if (!pokemon.hasOwnProperty(key))
+                    valid = false;
+            });              
+            if (valid)
+                pokedex.push(pokemon);
+            else
+                console.warn("Pokemon data is missing information.  Pokemon not added.");
+        } else {
+        console.warn("Pokemon of type " + typeof pokemon + " was not added to the repository.");
+        }
+    }
+
+    function getAll() {
+        return pokedex;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
+
+//The following entry will not work because the parameter's value is not an object
+console.log('TEST: The following entry will not work because the parameter\'s value is not an object');
+pokemonRepository.add('WillNotWorkExample');
+
+//The following entry will not work because this object is missing the required key 'height'
+console.log('TEST: The following entry will not work because this object is missing the required key \'height\'');
+pokemonRepository.add({
+    name: 'WontWorkExample',
+    types: ['missing', 'height']
+});
+
+pokemonRepository.add({
     name: 'Birobirio',
     height: 80,
     types: ['dust', 'wind', 'cacti']
 });
 
-repository.push({
+pokemonRepository.add({
     name: 'Chipozard',
     height: 2,
     types: ['bird', 'hooves', 'scales']
 });
 
-repository.push({
+pokemonRepository.add({
     name: 'Drikka',
     height: 12,
     types: ['fur', 'electricity', 'teeth']
 });
 
-document.write("<ul>");
-for (let i = 0; i < repository.length; i++) {
-    let comment = " - Normal";
-    if (repository[i].height < 5) comment = " - very small.";
-    if (repository[i].height > 50) comment = " - Wow, that's tall!";
+pokemonRepository.add({
+    name: 'Fworasword',
+    height: 15,
+    types: ['metal', 'fire', 'scales']
+});
 
-    document.write('<li><h2>' + repository[i].name + '</h2><span class="declaration">(height: ' + repository[i].height + comment + ')</span></li>');
-}
+pokemonRepository.add({
+    name: 'Gub',
+    height: 1,
+    types: ['ooze', 'tentacles', 'slippery']
+});
+
+document.write("<ul>");
+pokemonRepository.getAll().forEach(function (pokemon) {
+    let comment = " - Normal";
+    if (pokemon.height < 5) comment = " - very small.";
+    if (pokemon.height > 50) comment = " - Wow, that's tall!";
+
+    document.write('<li><h2>' + pokemon.name + '</h2><span class="declaration">(height: ' + pokemon.height + comment + ')</span></li>');
+});
 document.write("</ul>");
